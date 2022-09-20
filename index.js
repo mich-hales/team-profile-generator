6ed2b7750434412ddb4ca96fa9ad55a546b75cf9
mich-hales <michellehales93@gmail.com>
2022-09-20 14:13:42 -0600

    git ignore file updated

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
 const Engineer = require('./Engineer');
 const Intern = require('./Intern');
 const Manager = require('./Manager');
+
 
